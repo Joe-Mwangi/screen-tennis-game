c41eabe4a1b33a9682f2b0e29600b67f2986bf57
Joe-Mwangi <joem99271@gmail.com>
2023-03-15 06:22:33 +0300

    initial

---
+++ server.js
@@ -0,0 +1,12 @@
+const server = require('http').createServer()
+const io = require('socket.io')(server)
+
+
+const port = 4000
+
+server.listen(port)
+console.log(`Server listening on port ${port}...`)
+ 
+io.on('connection', socket => {
+    console.log('a user connected...')
+})
